163c622e0bbf54bba1ae5bc08f1eafc058e24f4a
itaruf <imane93100@hotmail.fr>
2021-11-21 01:28:32 +0100

    Updating

---
+++ Source/Sandbox/SandboxGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SandboxGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SANDBOX_API ASandboxGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
